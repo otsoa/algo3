0fd0f3c7b3cb96efdf5fbba7bf5ed7c01ce8e132
Otso Alho <otso.alho@reaktor.fi>
2013-04-16 19:16:39 +0300

    simplify

--- client.hs
+++ client.hs
@@ -1,16 +1,16 @@
 import Network
 import Network.Socket
 import Control.Monad
-import System.IO(Handle, hFlush, hPutChar)
+import System.IO(Handle, hFlush, hPutChar, hPutStr)
 import Data.Aeson
 import Data.Maybe
 import Data.Data
 import Data.Typeable
-import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Char8 as L
 
 main = do
   handle <- connectSocket "localhost" 9999
-  L.hPutStr handle $ L.pack "join"
+  hPutStr handle "echo foo\n"
   handleConnection handle
 
 connectSocket :: String -> Integer -> IO(Handle)
@@ -18,13 +18,6 @@ connectSocket host port = connectTo host (PortNumber $ fromInteger port)
 
 handleConnection :: Handle -> IO()
 handleConnection h = do
-  lines <- liftM (L.split '\n') $ L.hGetContents h
-  handleLines h lines
-
-handleLines ::  Handle -> [L.ByteString] -> IO()
-handleLines h [] = do 
-  putStrLn "Got empty line from server."
-  return ()
-handleLines h lines = do
-  putStrLn $ L.unpack $ head lines
-  handleLines h $ tail lines
+  line <- L.hGetLine h
+  putStrLn $ "server responded " ++ L.unpack line
+  handleConnection h
